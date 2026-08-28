fb68d56daf4388d40c0988890e20a40583271f0f
Alerrsi <alexis14102020@gmail.com>
2026-08-28 13:03:19 -0400

    changes in requirements.txt

--- models/SolarPanel.py
+++ models/SolarPanel.py
@@ -1,7 +1,12 @@
 from pydantic import BaseModel
-
+from main import app
 
 class SolarPanel(BaseModel):
     # Class  fields
     model: str
     output_power: float
+    state: float
+
+
+    @app.get("api/solarpanel")
+    def
